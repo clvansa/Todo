c3fc8487d264401e74577d507de12fb75d67e480
majd <clvansa@gmail.com>
2020-08-11 14:13:49 +0200

    Updates

---
+++ precache-manifest.f1c2cf42f14579676196d8b1c8a43f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26428c8cd25dd1c1035b917bcd530991",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "dc008458ca58b725b956",
+    "url": "/Todo/static/css/main.c248ab2a.chunk.css"
+  },
+  {
+    "revision": "6b0878d459b49213dcba",
+    "url": "/Todo/static/js/2.82954558.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.82954558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc008458ca58b725b956",
+    "url": "/Todo/static/js/main.e4539df8.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  }
+]);
